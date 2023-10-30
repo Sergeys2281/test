a32431b1df3382f2fb3d7f6ce7f8751dac79bbf9
sergeys2281 <sergeys2281@gmail.com>
2023-10-30 21:56:27 +0200

    Do while changed to while.

--- Lab6.cpp
+++ Lab6.cpp
@@ -11,9 +11,9 @@ int main()
     x = 3;
     step = 0.5;
 
-    do {
+    while (x <= 6) {
         Y = cos(pow(x, 2)) + pow(sin(x), 2) + 2;
         cout << "x = " << x << " | Y = " << Y << endl;
         x = x + step;
-    } while (x <= 6);
+    }
 }
