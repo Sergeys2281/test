9d6a2767ef9051c0c47bd7836c348234f921e245
Sergeys <sergeys2281@gmail.com>
2023-10-31 14:13:40 +0200

    Added new file

---
+++ lab7.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+
+int main()
+{
+    using namespace std;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int i, m, y, Z, P;
+
+    P = 0;
+    m = 2;
+
+    cout << "Введіть y - ";
+    cin >> y;
+
+    for (i = 1; i < 6; i++)
+    {
+        P = P * ln(m * y);
+    }
+
+    Z = cos(P)
+    cout << "Z = " << Z;
+}
